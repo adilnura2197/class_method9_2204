d55085fb71c84b4e34b74f07cb73ff9c04f49c89
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-04-22 16:59:17 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Dars:
+    def boshlash(self):
+        print("Dars boshlandi")
+
+    def tushuntirish(self):
+        self.boshlash()
+        print("Mavzu tushuntirildi")
+
+    def yakunlash(self):
+        self.tushuntirish()
+        print("Dars tugadi")
